e26921877fa9fe74036d5f2f2bbf4828c1a7089b
AprendendoLinux <magnatahp@gmail.com>
2022-07-18 12:57:52 -0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -5,25 +5,13 @@ RUN apt update && apt dist-upgrade -y && apt autoremove -y \
 	&& apt install --yes --no-install-recommends apache2 php php-mysql php-ldap \
 	php-xmlrpc php-imap curl php-curl php-gd php-mbstring php-xml php-apcu-bc \
 	php-cas php-intl php-zip php-bz2 cron wget ca-certificates jq libldap-2.4-2 \
-	libldap-common libsasl2-2 libsasl2-modules libsasl2-modules-db && \
+	libldap-common libsasl2-2 libsasl2-modules libsasl2-modules-db mariadb-client && \
 	apt-get clean all && rm -rf /var/lib/apt/lists/* && \
 	ln -sf /dev/stdout /var/log/apache2/access.log && \
 	ln -sf /dev/stderr /var/log/apache2/error.log && \
 	sed -i "s/#AddDefaultCharset/AddDefaultCharset/g" /etc/apache2/conf-enabled/charset.conf && \
 	echo "ServerSignature Off" >> /etc/apache2/apache2.conf \
-	&& sed -i "s/2M/500M/" /etc/php/7.4/apache2/php.ini \
-	&& sed -i "s/2M/500M/" /etc/php/7.4/cli/php.ini \
-	&& sed -i "s/2M/500M/" /usr/lib/php/7.4/php.ini-development \
-	&& sed -i "s/2M/500M/" /usr/lib/php/7.4/php.ini-production \
-	&& sed -i "s/2M/500M/" /usr/lib/php/7.4/php.ini-production.cli \
-	&& sed -i "s/2M/500M/" /usr/lib/php/7.4/php.ini-production.cli \
-	&& rm -f /var/www/html/index.html \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /etc/php/7.4/apache2/php.ini \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /etc/php/7.4/cli/php.ini \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /usr/lib/php/7.4/php.ini-development \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /usr/lib/php/7.4/php.ini-production \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /usr/lib/php/7.4/php.ini-production.cli \
-	&& sed -i "s/post_max_size = 8M/post_max_size = 100M/" /usr/lib/php/7.4/php.ini-production.cli
+	&& rm -f /var/www/html/index.html
 
 COPY glpi.sh /opt/
 COPY 000-default.conf /etc/apache2/sites-enabled/000-default.conf
--- glpi.sh
+++ glpi.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-## Controle de versao do GLPI por VARIAVEL
 [[ ! "$VERSION" ]] \
 	&& VERSION=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/latest | grep tag_name | cut -d '"' -f 4)
 
-if [[ -z "${TIMEZONE}" ]]; then echo "O TIMEZONE nao esta definido"; 
+if [[ -z "$TIMEZONE" ]]; then echo "O TIMEZONE nao esta definido"; 
 	else 
 		echo "date.timezone = \"$TIMEZONE\"" > /etc/php/7.4/apache2/conf.d/timezone.ini;
 		echo "date.timezone = \"$TIMEZONE\"" > /etc/php/7.4/cli/conf.d/timezone.ini;
 		rm /etc/localtime && ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+fi
+
+
+if [[ -z "$UPLOAD_MAX_FILESIZE" ]];
+then 
+		echo "O UPLOAD_MAX_FILESIZE nao esta definido"
+		export dbhost=$(cat /var/www/html/config/config_db.php | grep dbhost | awk '{print $4}' | cut -d"'" -f2)
+		export dbuser=$(cat /var/www/html/config/config_db.php | grep dbuser | awk '{print $4}' | cut -d"'" -f2)
+		export dbpassword=$(cat /var/www/html/config/config_db.php | grep dbpassword | awk '{print $4}' | cut -d"'" -f2)
+		export dbdefault=$(cat /var/www/html/config/config_db.php | grep dbdefault | awk '{print $4}' | cut -d"'" -f2)
+		mysql -h''$dbhost'' -u''$dbuser'' -p''$dbpassword'' -e "UPDATE $dbdefault.glpi_configs SET value = 2 WHERE glpi_configs.id = 220"
+
+	else
+		sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /etc/php/7.4/apache2/php.ini \
+		&& sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /etc/php/7.4/cli/php.ini \
+		&& sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-development \
+		&& sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production \
+		&& sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production.cli \
+		&& sed -i "s/2M/$UPLOAD_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production.cli
+		export dbhost=$(cat /var/www/html/config/config_db.php | grep dbhost | awk '{print $4}' | cut -d"'" -f2)
+		export dbuser=$(cat /var/www/html/config/config_db.php | grep dbuser | awk '{print $4}' | cut -d"'" -f2)
+		export dbpassword=$(cat /var/www/html/config/config_db.php | grep dbpassword | awk '{print $4}' | cut -d"'" -f2)
+		export dbdefault=$(cat /var/www/html/config/config_db.php | grep dbdefault | awk '{print $4}' | cut -d"'" -f2)
+		export valor=$(cat /etc/php/7.4/apache2/php.ini | grep max_filesize | awk '{print $3}' | cut -d'M' -f1)
+		mysql -h''$dbhost'' -u''$dbuser'' -p''$dbpassword'' -e "UPDATE $dbdefault.glpi_configs SET value = $valor WHERE glpi_configs.id = 220"
+fi
+
+if [[ -z "$POST_MAX_FILESIZE" ]]; then echo "O POST_MAX_FILESIZE nao esta definido";
+	else
+		sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /etc/php/7.4/apache2/php.ini \
+		&& sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /etc/php/7.4/cli/php.ini \
+		&& sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-development \
+		&& sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production \
+		&& sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production.cli \
+		&& sed -i "s/post_max_size = 8M/post_max_size = $POST_MAX_FILESIZE/" /usr/lib/php/7.4/php.ini-production.cli
 fi
 
 LINK_GLPI=$(curl -s https://api.github.com/repos/glpi-project/glpi/releases/tags/$VERSION | jq .assets[0].browser_download_url | tr -d \")
